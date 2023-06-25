5a9b33dbc82129d85767faedf36a46fe135c43c2
MimBinFikret <muhammed.samil.albayrak@gmail.com>
2023-06-25 05:55:26 +0300

    Restart

---
+++ dapie.py
@@ -0,0 +1,36 @@
+﻿import sys
+import subprocess
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget
+
+import docker
+
+class DockerApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Docker Konteynır Listesi")
+        self.setGeometry(100, 100, 400, 300)
+
+        self.docker_client = docker.from_env()
+        self.container_list = QListWidget(self)
+        self.container_list.setGeometry(10, 10, 380, 280)
+
+        self.update_container_list()
+
+    def update_container_list(self):
+        self.container_list.clear()
+
+        containers = self.docker_client.containers.list()
+        for container in containers:
+            self.container_list.addItem(container.name)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    
+    # Create the subprocess with CREATE_NO_WINDOW flag
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    subprocess.Popen([sys.executable, "dapie.pyw"], startupinfo=startupinfo)
+    
+    window = DockerApp()
+    window.show()
+    sys.exit(app.exec_())
